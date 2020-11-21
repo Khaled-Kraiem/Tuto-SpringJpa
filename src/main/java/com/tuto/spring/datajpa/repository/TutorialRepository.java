cc42fb511d43a7e53bdd185d2cf4c9f1e3e040ec
khaled kraiem <khaled.kraiem.91@gmail.com>
2020-11-21 23:19:51 +0100

    Spring Boot Pagination & Filter example

--- src/main/java/com/tuto/spring/datajpa/repository/TutorialRepository.java
+++ src/main/java/com/tuto/spring/datajpa/repository/TutorialRepository.java
@@ -2,11 +2,13 @@ package com.tuto.spring.datajpa.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tuto.spring.datajpa.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-  List<Tutorial> findByPublished(boolean published);
-  List<Tutorial> findByTitleContaining(String title);
+  Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+  Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
 }
